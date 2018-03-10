929b6e8a48ceaf8852241f0801f19ef6d8ac0cc4
爬上上坡 <pashangshangpo@gmail.com>
2018-03-10 21:44:04 +0800

    up data

--- js/init.js
+++ js/init.js
@@ -4,15 +4,6 @@
 
 const isMobile = () => {
  return navigator.userAgent.toLowerCase().indexOf('mobile') > -1
-<<<<<<< HEAD
-}
-
-// 优化展示效果, 部分内容不需要在移动端显示
-if (isMobile()) {
-  const elements = document.querySelectorAll('[data-pc]')
-  for (let el of elements) {
-    el.parentNode.removeChild(el)
-=======
 };
 
 // 优化展示效果, 部分内容不需要在移动端显示
@@ -20,6 +11,5 @@ if (isMobile()) {
   const elements = document.querySelectorAll('[data-pc]');
   for (let el of elements) {
     el.parentNode.removeChild(el);
->>>>>>> 57e62531054eea66c1d92f07f202d3bee25db6ef
   }
 }
