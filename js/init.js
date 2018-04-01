27d3ac1d12197de111de8d07d084e42efa2c0af3
爬上上坡 <pashangshangpo@gmail.com>
2018-04-01 17:45:09 +0800

    up data

---
+++ js/init.js
@@ -0,0 +1,15 @@
+/**
+ * 初始化
+ */
+
+const isMobile = () => {
+ return navigator.userAgent.toLowerCase().indexOf('mobile') > -1
+}
+
+// 优化展示效果, 部分内容不需要在移动端显示
+if (isMobile()) {
+  const elements = document.querySelectorAll('[data-pc]')
+  for (let el of elements) {
+    el.parentNode.removeChild(el)
+  }
+}
